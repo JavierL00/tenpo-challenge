1d478f594adafa0ad7f67c950d3bbfb66186b8f4
javierlujan <javier.lujan.121@outlook.es>
2025-04-30 12:58:33 -0400

    Feat: Rate limit

---
+++ src/main/java/com/tenpo/api/configuration/ratelimit/RateLimitFilter.java
@@ -0,0 +1,50 @@
+package com.tenpo.api.configuration.ratelimit;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class RateLimitFilter implements Filter {
+
+	private static final Integer NUMBER_PETITIONS = 2;
+	private static final Integer MINUTES_DURATION = 1;
+
+	private final ConcurrentHashMap<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+	private Bucket createNewBucket() {
+		Bandwidth limit =
+		 Bandwidth.classic(NUMBER_PETITIONS, Refill.intervally(NUMBER_PETITIONS, Duration.ofMinutes(MINUTES_DURATION)));
+		return Bucket.builder().addLimit(limit).build();
+	}
+
+	@Override
+	public void doFilter(
+	 ServletRequest request, ServletResponse response,
+	 FilterChain chain
+	) throws IOException, ServletException {
+
+		String ip = request.getRemoteAddr();
+		Bucket bucket = buckets.computeIfAbsent(ip, k -> createNewBucket());
+
+		if (bucket.tryConsume(1)) {
+			chain.doFilter(request, response);
+		}
+		else {
+			((HttpServletResponse) response).setStatus(429);
+			response.setContentType("application/json");
+			response.getWriter().write("{\"error\":\"Rate limit exceeded\"}");
+		}
+	}
+}
--- src/main/java/com/tenpo/api/configuration/web/WebConfig.java
+++ src/main/java/com/tenpo/api/configuration/web/WebConfig.java
@@ -1,9 +1,11 @@
 package com.tenpo.api.configuration.web;
 
+import com.tenpo.api.configuration.ratelimit.RateLimitFilter;
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -35,5 +37,14 @@ public class WebConfig implements WebMvcConfigurer {
 
 			response.getOutputStream().write(wrappedResponse.getCachedBody());
 		};
+	}
+
+	@Bean
+	public FilterRegistrationBean<RateLimitFilter> rateLimitFilterRegistration(RateLimitFilter filter) {
+		FilterRegistrationBean<RateLimitFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(filter);
+		registration.addUrlPatterns("/*");
+		registration.setOrder(1);
+		return registration;
 	}
 }
